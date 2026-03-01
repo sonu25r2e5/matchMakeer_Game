e232f72b3fea319bbb7ddc332e61031311ab5fbd
sonu25r2e5 <sk7018059@gmail.com>
2026-03-01 12:47:01 +0545

    roll a dice commin

---
+++ Roll Dice GamePlay/main.js
@@ -0,0 +1,13 @@
+// logic is implemneted here we need to understand that . 
+// we define the const term here okay now 
+const roll1Btn = document.getElementById("roll1");
+const roll2Btn = document.getElementById("roll2");
+
+const result1Span = document.getElementById("result1");
+
+const result2Span = document.getElementById("result2");
+
+const winnerH3 = document.getElementById("winner");
+
+
+
